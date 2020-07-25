55fe9c8c791b9a47c2ef5eda704b0743787d4304
Marcos Stefani <marcos.stefani@m4u.com.br>
2020-07-24 23:35:05 -0300

    load properties

--- pyyaml-properties/model/yaml.py
+++ pyyaml-properties/model/yaml.py
@@ -1,6 +1,30 @@
+import yaml, os
+
 class Yaml(object):
     def __init__(self, environment):
         self.environment = environment.name.lower()
 
     def name(self):
         return "application-{}.yaml".format(self.environment)
+
+    def absoluteName(self):
+        location = os.path.dirname(os.path.abspath(self.name()))
+        return os.path.join(location, 'properties/{}'.format(self.name()))
+
+    def load(self, prop):
+        items = [prop]
+        if '.' in prop:
+            items = prop.split('.')
+        return self.find(items)
+
+    def find(self, items):
+        with open(self.absoluteName()) as f:
+            docs = yaml.load_all(f, Loader=yaml.FullLoader)
+            for doc in docs:
+                for k, v in doc.items():
+                    if k != items[0]:
+                        continue
+                    for i in items[1:]:
+                        if isinstance(v, dict):
+                            v = v[i]
+                    return v
